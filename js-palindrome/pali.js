44582e1f0f594fa837c4a70e32aac894ecb3a247
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-09-02 15:33:47 +0200

    completamento esercizio Palindromo

---
+++ js-palindrome/pali.js
@@ -0,0 +1,29 @@
+// Chiedere all'utente di inserire una parola
+let word = prompt ("Inserisci una parola");
+
+// Invocare una funzione per invertire la parola scritta e verificare che sia palindroma
+function reversedWord(parola) {
+
+    //rendere la parola tutta minuscola in modo da facilitare il confronto
+    parola = parola.toLowerCase();
+    //var di stringa invertita
+    let revertedWord = "";
+
+    //ciclo la stringa
+    for (let i = parola.length - 1; i >= 0; i--){
+        let lettera = parola[i];
+        
+        revertedWord += lettera;
+    }
+    //verificare se la parola è palindroma con true o false
+    return parola === revertedWord;
+}
+// Mandare true o false in console
+console.log(reversedWord(word))
+
+// Chiamare la funzione e mandare in console il risultato
+if (reversedWord(word)){
+    console.log("La parola è palindroma")
+} else {
+    console.log("La parola non è palindroma")
+}
